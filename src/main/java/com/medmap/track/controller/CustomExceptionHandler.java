c7b4d7a62648201b4a429d6f1fd3a65b0a30f46a
Sahil Sonkar <sonkarsahil04@gmail.com>
2024-06-01 20:01:47 +0530

    handled error response correctly

---
+++ src/main/java/com/medmap/track/controller/CustomExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.medmap.track.controller;
+
+import com.medmap.track.exception.BadRequestException;
+import com.medmap.track.model.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException ex, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+}
